8dd2f32e3ca1f72609a1fab3c58a5e656e1b61d5
Harshyadav02 <harshyadav5736@gmail.com>
2022-12-15 15:13:26 +0530

    Function

---
+++ Function.py
@@ -0,0 +1,63 @@
+<<<<<<< HEAD
+## functions
+
+### def key word is used to create a function 
+
+##### syntax :- def < functionName >(parmeters or argumnts):
+				## body
+
+### area of rantagle by function
+
+def rect_area():
+	length = int(input("enter the length"))
+	width = int(input("enter the breadth"))
+	area = length * width
+	print(area)
+rect_area()
+
+### factorail by function 
+
+def factorial(m):
+	fact =1
+	if(m == 1):
+		return 1
+	else :
+		for i in range(1,m+1):
+			fact = fact*i
+	return fact
+for i in range(3):
+	choice = int(input("enter the choice "))
+	fact_value = factorial(choice)
+	print("factorail of ",choice ,"is = ",fact_value)
+=======
+## functions
+
+### def key word is used to create a function 
+
+##### syntax :- def < functionName >(parmeters or argumnts):
+				## body
+
+### area of rantagle by function
+
+def rect_area():
+	length = int(input("enter the length"))
+	width = int(input("enter the breadth"))
+	area = length * width
+	print(area)
+rect_area()
+
+### factorail by function 
+
+def factorial(m):
+	fact =1
+	if(m == 1):
+		return 1
+	else :
+		for i in range(1,m+1):
+			fact = fact*i
+	return fact
+for i in range(3):
+	choice = int(input("enter the choice "))
+	fact_value = factorial(choice)
+	print("factorail of ",choice ,"is = ",fact_value)
+>>>>>>> c94a28cd99a7eff4569b32fafacb8d8f2585a421
